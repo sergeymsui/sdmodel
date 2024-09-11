0ac7a6fc328553e842382871ba73ba91dcf33e45
Sergey Malichenko <s.malichenko@yandex.ru>
2024-09-11 22:39:14 +0300

    feat: update path

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="sdmodel",
     version="0.0",
     description="Models for finding equilibrium in a transport network",
-    packages=["", "grad_methods"],
+    packages=["sdmodel", "sdmodel/grad_methods"],
     author_email="kkb.boyko@gmail.com",
     zip_safe=False,
 )
